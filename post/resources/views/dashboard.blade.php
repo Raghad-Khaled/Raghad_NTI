66b864282bf0e627a58744a1210bf6b8eb7d0102
Raghad-Khaled <raghoda1234692000@gmail.com>
2022-09-29 14:28:31 +0200

    Task Done

--- post/resources/views/dashboard.blade.php
+++ post/resources/views/dashboard.blade.php
@@ -1,17 +1,91 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
+@extends('layouts.parent')
+@section('contant')
+    <h1 class="text-center"> Creat Post </h1>
+    <form action="{{ route('dashboard.posts.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" value="{{ old('title') }}" name="title" class="form-control" id="title">
+            @error('title')
+                <div class="alert alert-danger" role="alert">{{ $message }}</div>
+            @enderror
         </div>
+        <div class="form-group">
+            <label for="details_en">Details </label>
+            <textarea name="details" id="details" cols="10" rows="5" class="form-control">{{ old('details') }}</textarea>
+            @error('details')
+                <div class="alert alert-danger" role="alert">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="image">Post Image</label>
+            <input type="file" name="image" class="form-control" id="image">
+            @error('image')
+                <div class="alert alert-danger" role="alert">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="link">Youtube Link</label>
+            <input type="url" value="{{ old('link') }}" name="link" class="form-control" id="link">
+            @error('link')
+                <div class="alert alert-danger" role="alert">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">creat Post</button>
+    </form>
+
+
+    <div class="card mt-5">
+        <div class="col-12">
+            @if (session('success'))
+                <div class="alert alert-success text-center">
+                    {{ session('success') }}
+                </div>
+            @endif
+        </div>
+        <div class="card-header">
+            <h3 class="card-title">All Posts</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+
+                        {{-- @foreach ($products[0] as $key => $value)
+                            <th scope="col"> {{ $key }}</th>
+                        @endforeach --}}
+
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Operations</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{ $post->id }}</td>
+                            <td> <a href="{{route('dashboard.post.index', $post->id)}}"> {{ $post->title }} </a> </td>
+                            <td><img src="{{ asset('images/posts/' . $post->image) }}" class="w-20" alt="post image"></td>
+
+                            <td>
+                                <form action="{{ route('dashboard.posts.delete', $post->id) }}" method="post"
+                                    calss="d-inline">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-outline-danger">Delete</button>
+                                </form>
+
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
     </div>
-</x-app-layout>
+@endsection
--- post/routes/web.php
+++ post/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsContoller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard',[PostsContoller::class,'index'] )->middleware(['auth'])->name('dashboard');
+Route::post('/dashboard/posts/store',[PostsContoller::class,'store'] )->middleware(['auth'])->name('dashboard.posts.store');
+Route::delete('/dashboard/posts/delete/{id}',[PostsContoller::class,'destroy'] )->middleware(['auth'])->name('dashboard.posts.delete');
+Route::get('/dashboard/posts/{id}',[PostsContoller::class,'find'] )->middleware(['auth'])->name('dashboard.post.index');
 
 require __DIR__.'/auth.php';
